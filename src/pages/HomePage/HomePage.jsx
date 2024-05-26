e7003d48c5fa832814ecc1cf1b0337f7db02772e
fulaytar <fulaytar2405@gmail.com>
2024-05-27 00:16:10 +0300

    add details home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,6 @@
 import PageTitle from "../../components/PageTitle/PageTitle";
+import css from "./HomePage.module.css";
+import { CiLinkedin } from "react-icons/ci";
 
 export default function HomePage() {
   return (
@@ -9,6 +11,32 @@ export default function HomePage() {
           💁‍♀️
         </span>
       </PageTitle>
+      <div className={css.container}>
+        <h2 className={css.h2}>Manage all your contacts effortlessly.</h2>
+        <p className={css.p}>
+          <strong>Secure Login & Registration</strong>: Protect your contact
+          information with secure login and registration features.
+        </p>
+        <p className={css.p}>
+          <strong>Store & Organize Contacts</strong>: Easily save and categorize
+          all your contacts in one place.
+        </p>
+        <p className={css.p}>
+          <strong>User-Friendly Interface</strong>: Navigate and manage your
+          contacts with a simple and intuitive design.
+        </p>
+        <p className={css.p}>Start organizing your contacts today!</p>
+        <p>
+          Developed by{" "}
+          <a
+            className={css.link}
+            href="https://www.linkedin.com/in/ivan-fulaitar/"
+            target="_blank"
+          >
+            Ivan Fulaitar
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
